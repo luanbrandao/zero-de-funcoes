d6d0294c476719c768e1a03612ae421268eca5db
luanBrandao <luanbrandao4@gmail.com>
2019-11-07 11:01:58 -0300

    feat: add a page de exemplos as funcoes de entrada

--- src/app/utils/funcoes.ts
+++ src/app/utils/funcoes.ts
@@ -3,19 +3,19 @@ export abstract class Funcoes {
   _funcaoEscrita = '';
 
 
-  funcao = (x) => {
+  funcao = (X) => {
     console.log('escrita aqui');
-    const cos = x => Math.cos(x);
-    const sen = x => Math.sin(x);
-    const pow = (x, y) => Math.pow(x, y)
-    const abs = (x) => Math.abs(x)
-    const sqrt = (x) => Math.sqrt(x)
-    const exp = (x) => Math.exp(x)
-    const cbrt = (x) => Math.cbrt(x)
+    const cos = X => Math.cos(X);
+    const sen = X => Math.sin(X);
+    const pow = (X, y) => Math.pow(X, y)
+    const abs = (X) => Math.abs(X)
+    const sqrt = (X) => Math.sqrt(X)
+    const exp = (X) => Math.exp(X)
+    const cbrt = (X) => Math.cbrt(X)
 
-    const funcao = this._funcaoEscrita.replace("x", x);
+    const funcao = this._funcaoEscrita.replace("X", X);
     console.log("funcao =>", funcao)
-    return eval(funcao).toFixed(7);
+    return eval(funcao).toFiXed(7);
 
   }
 
